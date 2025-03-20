18d06eb4cf9358ee3cf96762517d26096f1a70b6
mitolyn-twds <mitolynsales@gmail.com>
2025-03-20 07:49:57 +0530

    TWDS
    mitolyn files added

---
+++ mitolyn/script.js
@@ -0,0 +1,20 @@
+ // Disable Right Click
+//  document.oncontextmenu = () => {
+//     alert("Hahaha...😂__DEVELOPER IS KISHAN__😎")
+//     return false;
+// }
+
+
+// document.addEventListener("contextmenu", function (e) {
+//     e.preventDefault(); // Disable right-click
+// });
+
+// document.addEventListener("keydown", function (e) {
+//     if (
+//         e.ctrlKey && (e.key.toLowerCase() === "u" || e.key.toLowerCase() === "i" || e.key.toLowerCase() === "j" || e.key.toLowerCase() === "s") ||
+//         e.key === "F12"
+//     ) {
+//         e.preventDefault();
+//         alert("Not again.....!😂");
+//     }
+// });
